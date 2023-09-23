827d458e2c3bbb4ad02446e30a30051f9a966db7
Bennyhinn18 <137780675+Bennyhinn18@users.noreply.github.com>
2023-09-23 20:53:21 +0530

    Create Input.java

---
+++ Input.java
@@ -0,0 +1,12 @@
+import java.util.Scanner
+class Input
+  {
+    Scanner S=new Scanner(System.in);
+    public static void main(String arg[])
+    {
+      String a;
+      a=s.next();
+      System.out.print("Entered string is " +a)
+        
+    }
+  }
