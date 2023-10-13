67599ea4002a58ccf86d3d96bb68edb7226539fa
Bennyhinn18 <137780675+Bennyhinn18@users.noreply.github.com>
2023-10-13 05:12:26 +0000

    greatest among three numbers program

--- New folder (2)/Greatest.java
+++ New folder (2)/Greatest.java
@@ -1,4 +1,17 @@
-class Greatest
-{
+import java.util.Scanner;
 
+class Greatest {
+    public static int findGreatest(int x, int y, int z) {
+        return (x > y) ? ((x > z) ? x : z) : ((y > z) ? y : z);
+    }
+    
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter three numbers: ");
+        int a = sc.nextInt();
+        int y = sc.nextInt();
+        int z = sc.nextInt();
+        int greatest = findGreatest(a, y, z);
+        System.out.println("The greatest number is " + greatest + ".");
+    }
 }
